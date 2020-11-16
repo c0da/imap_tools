c2137db44e24beb2f07a005ebf8295be1184069a
Coda <coda@coda.com.ar>
2020-11-16 11:47:30 +0100

    Trying to fix #68

--- imap_tools/message.py
+++ imap_tools/message.py
@@ -225,7 +225,10 @@ class MailMessage:
             if part.get_content_maintype() == 'multipart':
                 # multipart/* are just containers
                 continue
-            if part.get('Content-Disposition') is None and part.get('Content-ID') is None:
+            if part.get('Content-Disposition') is None and part.get('Content-ID') is None and part.get('Content-Transfer-Encoding') is None:
+                continue
+            attachment = Attachment(part)
+            if not attachment.filename:
                 continue
             results.append(Attachment(part))
         return results
